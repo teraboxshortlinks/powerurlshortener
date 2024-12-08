9a910397a22e52544d40c40aa367dbc7c9bf58a3
Jit <142555479+jit362@users.noreply.github.com>
2024-12-08 10:10:12 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -23,12 +23,19 @@ const bot = new TelegramBot(botToken, { polling: true });
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const username = msg.from.username;
-  const welcomeMessage = `Hello, ${username}!\n\n`
-    + 'Welcome to the URL Shortener Bot!\n'
-    + 'You can use this bot to shorten URLs using the AdlinkFly service.\n\n'
+  const welcomeMessage = `😇 Hello, ${username}!\n\n`
+    + 'Welcome to the Indishort URL Shortener Bot!\n'
+    + 'You can use this bot to shorten URLs using the Indishort.live api service.\n\n'
     + 'To shorten a URL, just type or paste the URL directly in the chat, and the bot will provide you with the shortened URL.\n\n'
-    + 'If you haven\'t set your AdlinkFly API token yet, use the command:\n/setapi YOUR_ADLINKFLY_API_TOKEN\n\n'
-    + 'Now, go ahead and try it out!';
+    + 'If you haven\'t set your Indishort API token yet, use the command:\n/setapi YOUR_Indishort_API_TOKEN\n\n'
+    + 'How To Use Me 👇👇 \n\n'
+  + '✅1. Got To https://indishort.live & Complete Your Registration.\n\n'
+  + '✅2. Then Copy Your API Key from here https://indishort.live/member/tools/api Copy Your API Only. \n\n'
+  + '✅3. Then add your API using command /setapi \n\n' 
+  + 'Example: /setapi c49399f821fc020161bc2a31475ec59f35ae5b4\n\n'
+  + '⚠️ You must have to send link with https:// or http://\n\n'
+  + '<b>Made with ❤️ By: @jit362</b>';
+  + '**Now, go ahead and try it out!**';
 
   bot.sendMessage(chatId, welcomeMessage);
 });
@@ -41,7 +48,7 @@ bot.onText(/\/setapi (.+)/, (msg, match) => {
   // Save the user's AdlinkFly API token to the database
   saveUserToken(chatId, userToken);
 
-  const response = `AdlinkFly API token set successfully. Your token: ${userToken}`;
+  const response = `Your Indishort API token set successfully. ✅️✅️ Your token is: ${userToken}`;
   bot.sendMessage(chatId, response);
 });
 
@@ -115,7 +122,7 @@ async function shortenUrl(chatId, url) {
   const adlinkflyToken = getUserToken(chatId);
 
   if (!adlinkflyToken) {
-    bot.sendMessage(chatId, 'Please provide your AdlinkFly API token first. Use the command: /setapi YOUR_ADLINKFLY_API_TOKEN');
+    bot.sendMessage(chatId, 'Please set up 🎃 your INDISHORT API token first. 🔮 Use the command: /setapi YOUR_INDISHORT_API_TOKEN');
     return null;
   }
 
